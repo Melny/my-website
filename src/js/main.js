a99c39eaf15f7b808177d66e226e6a6bf9591517
Melny <marek.melnycok@gmail.com>
2025-04-01 11:27:27 +0200

    Style changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 // This file is intentionally left blank.
+
+console.log("Hello World");
+
+const numberOne = 12;
+const numberTwo = 43;
+
+const myArray = [10, 20, 40];
+const myString = "Marek";
+
+function sum(a, b) {
+    return a + b;
+}
+
+for (const myValue of myArray) {
+    console.log(myValue);
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log(sum(numberOne, numberTwo));
+console.log(sum(numberOne, numberTwo));
+console.log(myArray[0]);
